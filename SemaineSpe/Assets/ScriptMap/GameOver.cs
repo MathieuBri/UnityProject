06b37c787ef71d0548789c19594007cc840c5402
Mathieu BRIAND <briandm@iut-acy.local>
2019-06-07 11:52:37 +0200

    mat 11h39

--- SemaineSpe/Assets/ScriptMap/GameOver.cs
+++ SemaineSpe/Assets/ScriptMap/GameOver.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour {
 
@@ -10,11 +11,18 @@ public class GameOver : MonoBehaviour {
     public BoxCollider chest;
 
     private AudioSource[] sounds;
+    private float timer;
     private void Start()
     {
         sounds = GetComponents<AudioSource>();
         sounds[0].volume = PlayerPrefs.GetFloat("volume") / 100;
         sounds[1].volume = PlayerPrefs.GetFloat("volume") / 100;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        GameObject.Find("Time Value").GetComponent<Text>().text = System.Math.Round(timer, 1).ToString();
     }
     private void OnCollisionEnter(Collision collision)
     {
